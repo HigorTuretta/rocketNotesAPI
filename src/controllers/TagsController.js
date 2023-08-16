255a5114a26c36e423e1af1dbddac0bea6c4a5a3
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-08-16 17:39:28 -0300

    some Library Updates

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -4,7 +4,7 @@ class TagsController {
   async index(req, res) {
     const user_id = req.user.id;
 
-    const tags = await knex("tags").where({ user_id });
+    const tags = await knex("tags").where({ user_id }).groupBy('name');
 
     return res.json(tags);
   }
