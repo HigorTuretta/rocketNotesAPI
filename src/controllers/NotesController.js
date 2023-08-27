6a6fee38c3daac8e8a80752578be0e0b25ac3d3a
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-08-27 11:11:42 -0300

    Correção do bug whereLIKE

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -62,27 +62,30 @@ class NotesController {
   async index(req, res) {
     const { title, tags } = req.query;
     const user_id = req.user.id;
-
+    const colationRaw = knex.raw('COLLATE utf8mb4_general_ci');
     let notes;
 
     if (tags) {
       const filterTags = tags.split(",").map((tag) => tag.trim());
 
       notes = await knex("tags")
-        .select(["notes.id", "notes.title", "notes.user_id"])
-        .where("notes.user_id", user_id)
-        .whereLike("notes.title", `%${title}%`)
-        .whereIn("name", filterTags)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .groupBy("notes.id")
-        .orderBy("notes.title");
-    } else {
-      notes = await knex("notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`)
-        .orderBy("title");
-    }
-
+      .select(["notes.id", "notes.title", "notes.user_id"])
+      .where("notes.user_id", user_id)
+      .where(function () {
+        this.whereRaw(`notes.title ${colationRaw} ILIKE ?`, [`%${title}%`]);
+      })
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .groupBy("notes.id")
+      .orderBy("notes.title");
+  } else {
+    notes = await knex("notes")
+      .where({ user_id })
+      .where(function () {
+        this.whereRaw(`title ${colationRaw} LIKE ?`, [`%${title}%`]);
+      })
+      .orderBy("title");
+  }
     const userTags = await knex("tags").where({ user_id });
     const notesWithTags = notes.map((note) => {
       const noteTags = userTags.filter((tag) => tag.note_id === note.id);
